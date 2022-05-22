2293c6603afb378ee6ac172e4a670ae0cf6d4c9d
cola314 <ubj0314@naver.com>
2022-05-22 16:55:08 +0900

    refac: 비디오 다운로드 코드 뷰모델에서 VideoDownloader로 분리

---
+++ SimpleYoutubeDownloader/Services/VideoDownloader.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReactiveUI;
+using YoutubeExplode;
+
+namespace SimpleYoutubeDownloader.Services
+{
+    public class VideoDownloader
+    {
+        private const string VIDEO_FILE = "video.mp4";
+        private const string AUDIO_FILE = "audio.mp4";
+
+        private readonly BehaviorSubject<int> _progressSubject;
+
+        private readonly Subject<string> _statusSubject;
+
+        private readonly Logger _logger;
+
+        public VideoDownloader(Logger logger)
+        {
+            _logger = logger;
+            _statusSubject = new Subject<string>();
+            _progressSubject = new BehaviorSubject<int>(0);
+        }
+
+        public IObservable<string> Status => _statusSubject.AsObservable();
+        public IObservable<int> Progress => _progressSubject.AsObservable();
+
+        public Task DownloadVideoAsync(string url, string downloadFile)
+        {
+            _logger.WriteLine($"Download Url : {url}");
+            _logger.WriteLine($"Download Path : {downloadFile}");
+
+            // search video
+            SetStatus("비디오 검색 중");
+
+            return Task.Run(async () =>
+            {
+                var youtube = new YoutubeClient();
+                var video = await youtube.Videos.GetAsync(url);
+                _logger.WriteLine($"id : {video.Id}\ntitle : {video.Title}\nAuthor : {video.Author}");
+
+                var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+                _logger.WriteLine($"{streamManifest.Streams.Count} numbers of stream found");
+
+                var audioStreamInfo = streamManifest.GetAudioOnlyStreams()
+                    .Where(x => x.Container.Name == "mp4")
+                    .OrderByDescending(x => x.Bitrate.BitsPerSecond)
+                    .FirstOrDefault();
+
+                var videoStreamInfo = streamManifest.GetVideoOnlyStreams()
+                    .Where(x => x.Container.Name == "mp4")
+                    .OrderByDescending(x => x.VideoQuality.MaxHeight)
+                    .FirstOrDefault();
+
+                if (audioStreamInfo == null)
+                {
+                    _logger.WriteLine("Audio stream info not found");
+                    throw new Exception("다운로드 가능한 오디오가 없습니다");
+                }
+
+                if (videoStreamInfo == null)
+                {
+                    _logger.WriteLine("Video stream info not found");
+                    throw new Exception("다운로드 가능한 비디오가 없습니다");
+                }
+
+                // download video
+                _logger.WriteLine("Start download audio and video from server");
+
+                var videoDownload = Observable.Create<double>(async observer =>
+                    {
+                        await youtube.Videos.Streams.DownloadAsync(videoStreamInfo, VIDEO_FILE,
+                            new Progress<double>(observer.OnNext));
+                        observer.OnCompleted();
+                        return Disposable.Empty;
+                    })
+                    .Select(x => (int) (x * 100))
+                    .Distinct(progress => progress / 5)
+                    .Publish();
+
+                var audioDownload = Observable.Create<double>(async observer =>
+                    {
+                        await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, AUDIO_FILE,
+                            new Progress<double>(observer.OnNext));
+                        observer.OnCompleted();
+                        return Disposable.Empty;
+                    })
+                    .Select(x => (int) (x * 100))
+                    .Distinct(progress => progress / 5)
+                    .Publish();
+
+                videoDownload
+                    .Subscribe(progress => _logger.WriteLine($"Video download progress : {progress}%"));
+
+                audioDownload
+                    .Subscribe(progress => _logger.WriteLine($"Audio download progress : {progress}%"));
+
+                audioDownload.CombineLatest(videoDownload, (x, y) => (x + y) / 2)
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Subscribe(totalProgress =>
+                    {
+                        SetProgress(totalProgress);
+                        SetStatus($"다운로드 중 {totalProgress}% 완료");
+                    });
+
+                videoDownload.Connect();
+                audioDownload.Connect();
+
+                await Task.WhenAll(videoDownload.ToTask(), audioDownload.ToTask());
+                _logger.WriteLine("Finish download audio and video from server");
+
+                // combine audio and video
+                SetStatus("mp4 변환 작업 중");
+                new AudioCombineService(_logger).CombineAudioToVideo(AUDIO_FILE, VIDEO_FILE, downloadFile);
+
+                SetProgress(100);
+                SetStatus("다운로드 완료");
+                MessageBox.Show("다운로드 완료", "정보", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Process.Start("explorer.exe", Path.GetDirectoryName(downloadFile));
+
+                // clean up
+                _logger.WriteLine("Clean temp audio and video file");
+                File.Delete(VIDEO_FILE);
+                File.Delete(AUDIO_FILE);
+                _logger.WriteLine("Finish Clean temp audio and video file");
+            });
+        }
+
+        private void SetStatus(string message)
+        {
+            _statusSubject.OnNext(message);
+        }
+
+        private void SetProgress(int value)
+        {
+            _progressSubject.OnNext(value);
+        }
+    }
+}
--- SimpleYoutubeDownloader/ViewModels/MainViewModel.cs
+++ SimpleYoutubeDownloader/ViewModels/MainViewModel.cs
@@ -1,46 +1,31 @@
 ﻿using ReactiveUI;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Reactive;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Reactive.Threading.Tasks;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using SimpleYoutubeDownloader.Services;
-using YoutubeExplode;
 
 namespace SimpleYoutubeDownloader.ViewModels
 {
     public class MainViewModel : ReactiveObject
     {
-        private const string VIDEO_FILE = "video.mp4";
-        private const string AUDIO_FILE = "audio.mp4";
         private readonly Logger _logger = Logger.Instance;
 
         public MainViewModel()
         {
-            DownloadCommand = ReactiveCommand.Create(() => DownloadVideo());
-            _statusSubject = new BehaviorSubject<string>("진행상황");
-            _statusText = _statusSubject.ToProperty(this, x => x.StatusText, scheduler: RxApp.MainThreadScheduler);
+            DownloadCommand = ReactiveCommand.Create(DownloadVideo);
         }
 
-        private readonly BehaviorSubject<string> _statusSubject;
+        private string _statusText;
 
-        public void SetStatus(string status)
+        public string StatusText
         {
-            _statusSubject.OnNext(status);
+            get => _statusText;
+            set => this.RaiseAndSetIfChanged(ref _statusText, value);
         }
-
-        private readonly ObservableAsPropertyHelper<string> _statusText; 
-        public string StatusText => _statusText.Value;
 
         private string _searchText;
+
         public string SearchText
         {
             get => _searchText;
@@ -48,6 +33,7 @@ namespace SimpleYoutubeDownloader.ViewModels
         }
 
         private int _progress;
+
         public int Progress
         {
             get => _progress;
@@ -55,6 +41,7 @@ namespace SimpleYoutubeDownloader.ViewModels
         }
 
         private bool _downloadEnable;
+
         public bool DownloadEnable
         {
             get => _downloadEnable;
@@ -63,142 +50,46 @@ namespace SimpleYoutubeDownloader.ViewModels
 
         public ReactiveCommand<Unit, Unit> DownloadCommand { get; }
 
-        private void DownloadVideo()
+        private async void DownloadVideo()
         {
-            if (this.DownloadEnable)
-            {
-                return;
-            }
+            if (DownloadEnable) return;
+            DownloadEnable = true;
 
-            this.DownloadEnable = true;
-            SetStatus("다운로드 시작");
-            _logger.WriteLine("Start Download");
             var saveDialog = new SaveFileDialog();
             saveDialog.DefaultExt = ".mp4";
             saveDialog.Filter = "MPEG-4 Video|*.mp4";
 
             if (saveDialog.ShowDialog() == DialogResult.OK)
             {
-                string downloadPath = SearchText;
-                string targetFileName = saveDialog.FileName;
+                StatusText = "다운로드 시작";
+                _logger.WriteLine("Start Download");
 
-                _logger.WriteLine($"Download Path : {downloadPath}");
-                _logger.WriteLine($"Target File : {targetFileName}");
+                var downloader = new VideoDownloader(_logger);
+                downloader.Status
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Subscribe(x => StatusText = x);
 
-                Task.Run(async () =>
+                downloader.Progress
+                    .ObserveOn(RxApp.MainThreadScheduler)
+                    .Subscribe(x => Progress = x);
+                try
                 {
-                    try
-                    {
-                        // search video
-                        SetStatus("비디오 검색 중");
-                        _logger.WriteLine($"Search Video Info");
-                        _logger.WriteLine($"URL : \"{downloadPath}\"\nSaveFile : \"{targetFileName}\"");
-
-                        var youtube = new YoutubeClient();
-                        var video = await youtube.Videos.GetAsync(downloadPath);
-                        _logger.WriteLine($"id : {video.Id}\ntitle : {video.Title}\nAuthor : {video.Author}");
-
-                        var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
-                        _logger.WriteLine($"{streamManifest.Streams.Count} numbers of stream found");
-
-                        var audioStreamInfo = streamManifest.GetAudioOnlyStreams()
-                            .Where(x => x.Container.Name == "mp4")
-                            .OrderByDescending(x => x.Bitrate.BitsPerSecond)
-                            .FirstOrDefault();
-
-                        var videoStreamInfo = streamManifest.GetVideoOnlyStreams()
-                            .Where(x => x.Container.Name == "mp4")
-                            .OrderByDescending(x => x.VideoQuality.MaxHeight)
-                            .FirstOrDefault();
-
-                        if (audioStreamInfo == null)
-                        {
-                            _logger.WriteLine("Audio stream info not found");
-                            throw new Exception("다운로드 가능한 오디오가 없습니다");
-                        }
-
-                        if (videoStreamInfo == null)
-                        {
-                            _logger.WriteLine("Video stream info not found");
-                            throw new Exception("다운로드 가능한 비디오가 없습니다");
-                        }
-
-                        // download video
-                        _logger.WriteLine("Start download audio and video from server");
-
-                        var videoDownload = Observable.Create<double>(async observer =>
-                            {
-                                await youtube.Videos.Streams.DownloadAsync(videoStreamInfo, VIDEO_FILE,
-                                    new Progress<double>(observer.OnNext));
-                                observer.OnCompleted();
-                                return Disposable.Empty;
-                            })
-                            .Select(x => (int)(x * 100))
-                            .Distinct(progress => progress / 5)
-                            .Publish();
-
-                        var audioDownload = Observable.Create<double>(async observer =>
-                            {
-                                await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, AUDIO_FILE,
-                                    new Progress<double>(observer.OnNext));
-                                observer.OnCompleted();
-                                return Disposable.Empty;
-                            })
-                            .Select(x => (int)(x * 100))
-                            .Distinct(progress => progress / 5)
-                            .Publish();
-
-                        videoDownload
-                            .Subscribe(progress => _logger.WriteLine($"Video download progress : {progress}%"));
-
-                        audioDownload
-                            .Subscribe(progress => _logger.WriteLine($"Audio download progress : {progress}%"));
-
-                        audioDownload.CombineLatest(videoDownload, (x, y) => (x + y) / 2)
-                            .ObserveOn(RxApp.MainThreadScheduler)
-                            .Subscribe(totalProgress =>
-                            {
-                                this.Progress = totalProgress;
-                                SetStatus($"다운로드 중 {totalProgress}% 완료");
-                            });
-
-                        videoDownload.Connect();
-                        audioDownload.Connect();
-                        
-                        await Task.WhenAll(videoDownload.ToTask(), audioDownload.ToTask());
-                        _logger.WriteLine("Finish download audio and video from server");
-
-                        // combine audio and video
-                        SetStatus("mp4 변환 작업 중");
-                        new AudioCombineService(_logger).CombineAudioToVideo(AUDIO_FILE, VIDEO_FILE, targetFileName);
-
-                        this.Progress = 100;
-                        SetStatus("다운로드 완료");
-                        MessageBox.Show("다운로드 완료", "정보", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                        Process.Start("explorer.exe", Path.GetDirectoryName(targetFileName));
-
-                        // clean up
-                        _logger.WriteLine("Clean temp audio and video file");
-                        File.Delete(VIDEO_FILE);
-                        File.Delete(AUDIO_FILE);
-                        _logger.WriteLine("Finish Clean temp audio and video file");
-                    }
-                    catch (Exception ex)
-                    {
-                        SetStatus("다운로드 실패");
-                        _logger.WriteLine(ex.ToString());
-                        MessageBox.Show("다운로드 실패", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    this.DownloadEnable = false;
-                });
+                    await downloader.DownloadVideoAsync(SearchText, saveDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    StatusText = "다운로드 실패";
+                    _logger.WriteLine(ex.ToString());
+                    MessageBox.Show("다운로드 실패", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
             else
             {
-                SetStatus("다운로드 취소");
+                StatusText = "다운로드 취소";
                 _logger.WriteLine("Cancel Download");
-                this.DownloadEnable = false;
             }
+
+            DownloadEnable = false;
         }
     }
 }
